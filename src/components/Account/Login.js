34e2b3ee124d4a71be9e2dcd935e8aaf2809fbe3
kizwond <kizwond@gmail.com>
2020-11-03 11:10:12 +0900

    delete non use

--- src/components/Account/Login.js
+++ src/components/Account/Login.js
@@ -1,4 +1,4 @@
-import { Form, Input, Button, Checkbox, Divider } from 'antd';
+import { Form, Input, Button } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import './Login.css'
 
