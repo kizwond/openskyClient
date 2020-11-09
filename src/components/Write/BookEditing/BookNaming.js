1f3cca3fe2bf67d89096c1cb5bcf0bc502acc577
kizwond <kizwond@gmail.com>
2020-11-09 17:47:53 +0900

    test

--- src/components/Write/BookEditing/BookNaming.js
+++ src/components/Write/BookEditing/BookNaming.js
@@ -98,7 +98,7 @@ const HorizontalLoginForm = () => {
             rules={[{ required: false, message: '카테고리를 선택해 주세요' }]}
           >
             <Select placeholder="(미지정)">
-              {data.length > 0 ? data.map((category)=>(
+              {data.length > 0 && data.length !== undefined ? data.map((category)=>(
                                     <Option key={category._id} value={category.category_name}>{category.category_name}</Option>
                                   )) : <Option value="미지정">미지정</Option>}
             </Select>
