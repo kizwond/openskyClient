99de4549241f829dfdeadbaaacd8c95a3f1d6f27
kizwond <kizwond@gmail.com>
2020-11-11 14:03:42 +0900

    study page navlink css change

--- src/components/Study/Study.js
+++ src/components/Study/Study.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { NavLink} from 'react-router-dom';
 import { Layout, Menu, Breadcrumb, Dropdown, Space } from 'antd';
-import { DownOutlined } from '@ant-design/icons';
+import { AppstoreOutlined } from '@ant-design/icons';
 import './Study.css'
 const { Header, Content } = Layout;
 const { SubMenu } = Menu;
@@ -10,11 +11,18 @@ class Study extends Component {
     super(props);
     this.state = {  };
   }
+  onMenuClick = () => {
+    document.getElementById("nav_bar").classList.remove('nav_bar_hidden');
+    document.getElementById("nav_bar").classList.add('nav_bar');
+  }
   render() {
     const menu_0 = (
       <Menu>
         <Menu.ItemGroup>
-          <Menu.Item>앞/뒷면 서식 설정</Menu.Item>
+          <Menu.Item><NavLink to="/" exact onClick={this.onMenuClick}>메인</NavLink></Menu.Item>
+          <Menu.Item><NavLink to="/write" exact onClick={this.onMenuClick}>만들기</NavLink></Menu.Item>
+          <Menu.Item><NavLink to="/store" exact onClick={this.onMenuClick}>서점</NavLink></Menu.Item>
+          <Menu.Item>로그아웃</Menu.Item>
         </Menu.ItemGroup>
       </Menu>
     );
@@ -52,29 +60,36 @@ class Study extends Component {
     return (
       <div className="study_page_container">
         <Layout className="layout">
-          <Header style={{background:'white', paddingLeft:100}}>
-            <Dropdown overlay={menu_0} >
-              <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-                보기
-              </a>
-            </Dropdown>
-          <Space size='large'>
-              <Dropdown overlay={menu_1} >
+          <Header style={{background:'white', paddingLeft:10}}>
+            <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+              <Dropdown overlay={menu_0} >
                 <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-                  보기
+                <AppstoreOutlined style={{marginRight:'100px'}}/>
                 </a>
               </Dropdown>
-              <Dropdown overlay={menu_2} >
-                <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-                  서식
-                </a>
-              </Dropdown>
-              <Dropdown overlay={menu_3} >
-                <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
-                  학습
-                </a>
-              </Dropdown>
-            </Space>
+              <div style={{display:'flex', width:'90%', flexDirection:'row', justifyContent:'space-between'}}>
+              <Space size='large'>
+                <Dropdown overlay={menu_1} >
+                  <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                    보기
+                  </a>
+                </Dropdown>
+                <Dropdown overlay={menu_2} >
+                  <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                    서식
+                  </a>
+                </Dropdown>
+                <Dropdown overlay={menu_3} >
+                  <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                    학습
+                  </a>
+                </Dropdown>
+              </Space>
+              <Menu>
+                  <Menu.Item>학습종료</Menu.Item>
+                </Menu>
+              </div>
+            </div>
           </Header>
           <Content style={{ padding: '100px 50px' }}>
             <div className="site-layout-content">Content</div>
