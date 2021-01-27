876dae4c094f6e5a785eecb16a16232d8c210e18
kizwond <kizwond@gmail.com>
2021-01-27 14:39:13 +0900

    study result markup

--- src/components/Study/FinishStudy.js
+++ src/components/Study/FinishStudy.js
@@ -25,7 +25,6 @@ class FinishStudy extends Component {
 
     }
 
-
     render() { 
         console.log(this.state.result)
         console.log(this.state.by_book)
@@ -33,13 +32,15 @@ class FinishStudy extends Component {
            const total_result = this.state.result.total
            const read_result = this.state.result.read
            const flip_result = this.state.result.flip
+           var average_level_before = this.state.result.avg_level.before
+           var average_level_after = this.state.result.avg_level.after
 
            var total_studied_cards_total = total_result.studied_cards.total
            var total_studied_cards_yet = total_result.studied_cards.yet
            var total_studied_cards_ing = total_result.studied_cards.ing
            var total_studied_cards_hold = total_result.studied_cards.hold
            var total_studied_cards_completed = total_result.studied_cards.completed
-           var total_studied_study_hour = total_result.study_hour
+           var total_studied_study_hour = total_result.study_hour / 1000 / 60
            var total_studied_diffi1 = total_result.study_times.diffi1
            var total_studied_diffi2 = total_result.study_times.diffi2
            var total_studied_diffi3 = total_result.study_times.diffi3
@@ -53,7 +54,7 @@ class FinishStudy extends Component {
            var read_studied_cards_ing = read_result.studied_cards.ing
            var read_studied_cards_hold = read_result.studied_cards.hold
            var read_studied_cards_completed = read_result.studied_cards.completed
-           var read_studied_study_hour = read_result.study_hour
+           var read_studied_study_hour = read_result.study_hour / 1000 / 60
            var read_studied_diffi1 = read_result.study_times.diffi1
            var read_studied_diffi2 = read_result.study_times.diffi2
            var read_studied_diffi3 = read_result.study_times.diffi3
@@ -67,7 +68,7 @@ class FinishStudy extends Component {
            var flip_studied_cards_ing = flip_result.studied_cards.ing
            var flip_studied_cards_hold = flip_result.studied_cards.hold
            var flip_studied_cards_completed = flip_result.studied_cards.completed
-           var flip_studied_study_hour = flip_result.study_hour
+           var flip_studied_study_hour = flip_result.study_hour / 1000 / 60
            var flip_studied_diffi1 = flip_result.study_times.diffi1
            var flip_studied_diffi2 = flip_result.study_times.diffi2
            var flip_studied_diffi3 = flip_result.study_times.diffi3
@@ -78,6 +79,9 @@ class FinishStudy extends Component {
 
 
         } else {
+            average_level_before = 0
+            average_level_after = 0
+
            total_studied_cards_total = 0
            total_studied_cards_yet = 0
            total_studied_cards_ing = 0
@@ -125,7 +129,8 @@ class FinishStudy extends Component {
                 const study_date = item.study_date
                 const book_id = item.book_id
                 const total_exp_gained = item.total.exp_gained
-                const total_study_hour = item.total.study_hour
+                const total_study_hour = item.total.study_hour / 1000 / 60;
+                console.log(total_study_hour)
 
                 const total_studied_cards_total = item.total.studied_cards.total
                 const total_studied_cards_yet = item.total.studied_cards.yet
@@ -175,7 +180,7 @@ class FinishStudy extends Component {
                             <li style={{marginTop:"5px"}}>책 제목 : {book_id}</li>
                             <li style={{marginTop:"5px"}}>학습날짜 : {study_date}</li>
                             <li style={{marginTop:"5px"}}>획득 경험치총합 : {total_exp_gained}exp</li>
-                            <li style={{marginTop:"5px"}}>학습시간 총합 : {total_study_hour}</li>
+                            <li style={{marginTop:"5px"}}>학습시간 총합 : {total_study_hour}분</li>
                         </ul>
 
                         <ul>
@@ -185,7 +190,7 @@ class FinishStudy extends Component {
                             <li style={{marginTop:"5px"}}>복습카드 토탈 : {total_studied_cards_ing}</li>
                             <li style={{marginTop:"5px"}}>보류카드 토탈 : {total_studied_cards_hold}</li>
                             <li style={{marginTop:"5px"}}>완료카드 토탈 : {total_studied_cards_completed}</li>
-                            <li style={{marginTop:"5px"}}>학습시간 : {total_studied_study_hour}</li>
+                            <li style={{marginTop:"5px"}}>학습시간 : {total_studied_study_hour}분</li>
                             <li style={{marginTop:"5px"}}>난이도1선택 : {total_studied_diffi1}회</li>
                             <li style={{marginTop:"5px"}}>난이도2선택 : {total_studied_diffi2}회</li>
                             <li style={{marginTop:"5px"}}>난이도3선택 : {total_studied_diffi3}회</li>
@@ -201,7 +206,7 @@ class FinishStudy extends Component {
                             <li style={{marginTop:"5px"}}>복습카드 토탈 : {read_studied_cards_ing}</li>
                             <li style={{marginTop:"5px"}}>보류카드 토탈 : {read_studied_cards_hold}</li>
                             <li style={{marginTop:"5px"}}>완료카드 토탈 : {read_studied_cards_completed}</li>
-                            <li style={{marginTop:"5px"}}>학습시간 : {read_studied_study_hour}</li>
+                            <li style={{marginTop:"5px"}}>학습시간 : {read_studied_study_hour}분</li>
                             <li style={{marginTop:"5px"}}>난이도1선택 : {read_studied_diffi1}회</li>
                             <li style={{marginTop:"5px"}}>난이도2선택 : {read_studied_diffi2}회</li>
                             <li style={{marginTop:"5px"}}>난이도3선택 : {read_studied_diffi3}회</li>
@@ -217,7 +222,7 @@ class FinishStudy extends Component {
                             <li style={{marginTop:"5px"}}>복습카드 토탈 : {flip_studied_cards_ing}</li>
                             <li style={{marginTop:"5px"}}>보류카드 토탈 : {flip_studied_cards_hold}</li>
                             <li style={{marginTop:"5px"}}>완료카드 토탈 : {flip_studied_cards_completed}</li>
-                            <li style={{marginTop:"5px"}}>학습시간 : {flip_studied_study_hour}</li>
+                            <li style={{marginTop:"5px"}}>학습시간 : {flip_studied_study_hour}분</li>
                             <li style={{marginTop:"5px"}}>난이도1선택 : {flip_studied_diffi1}회</li>
                             <li style={{marginTop:"5px"}}>난이도2선택 : {flip_studied_diffi2}회</li>
                             <li style={{marginTop:"5px"}}>난이도3선택 : {flip_studied_diffi3}회</li>
@@ -237,6 +242,7 @@ class FinishStudy extends Component {
             {this.state.result ? <div style={{width:'920px', margin:"auto", marginTop:"5%"}}>
                 <div style={{textAlign:"center", marginBottom:"5px", fontSize:"14px"}}>학습이 종료되었습니다!!!</div>
                 <div style={{textAlign:"center", marginBottom:"20px", fontSize:"14px"}}>수고하셨습니다!!!</div>
+                <div style={{textAlign:"center", marginBottom:"20px", fontSize:"14px"}}>평균레벨 이전 : {average_level_before} , 현재 : {average_level_after} </div>
                 <div style={{fontSize:"12px", display:"flex",flexDirection:"row", justifyContent:"space-between", width:'100%', marginBottom:"10px"}}>
                     <div>
                         <span>학습토탈</span>
@@ -246,7 +252,7 @@ class FinishStudy extends Component {
                             <li style={{marginTop:"5px"}}>복습카드 토탈 : {total_studied_cards_ing}</li>
                             <li style={{marginTop:"5px"}}>보류카드 토탈 : {total_studied_cards_hold}</li>
                             <li style={{marginTop:"5px"}}>완료카드 토탈 : {total_studied_cards_completed}</li>
-                            <li style={{marginTop:"5px"}}>학습시간 : {total_studied_study_hour}</li>
+                            <li style={{marginTop:"5px"}}>학습시간 : {total_studied_study_hour}분</li>
                             <li style={{marginTop:"5px"}}>난이도1선택 : {total_studied_diffi1}회</li>
                             <li style={{marginTop:"5px"}}>난이도2선택 : {total_studied_diffi2}회</li>
                             <li style={{marginTop:"5px"}}>난이도3선택 : {total_studied_diffi3}회</li>
@@ -264,7 +270,7 @@ class FinishStudy extends Component {
                             <li style={{marginTop:"5px"}}>복습카드 토탈 : {read_studied_cards_ing}</li>
                             <li style={{marginTop:"5px"}}>보류카드 토탈 : {read_studied_cards_hold}</li>
                             <li style={{marginTop:"5px"}}>완료카드 토탈 : {read_studied_cards_completed}</li>
-                            <li style={{marginTop:"5px"}}>학습시간 : {read_studied_study_hour}</li>
+                            <li style={{marginTop:"5px"}}>학습시간 : {read_studied_study_hour}분</li>
                             <li style={{marginTop:"5px"}}>난이도1선택 : {read_studied_diffi1}회</li>
                             <li style={{marginTop:"5px"}}>난이도2선택 : {read_studied_diffi2}회</li>
                             <li style={{marginTop:"5px"}}>난이도3선택 : {read_studied_diffi3}회</li>
@@ -282,7 +288,7 @@ class FinishStudy extends Component {
                             <li style={{marginTop:"5px"}}>복습카드 토탈 : {flip_studied_cards_ing}</li>
                             <li style={{marginTop:"5px"}}>보류카드 토탈 : {flip_studied_cards_hold}</li>
                             <li style={{marginTop:"5px"}}>완료카드 토탈 : {flip_studied_cards_completed}</li>
-                            <li style={{marginTop:"5px"}}>학습시간 : {flip_studied_study_hour}</li>
+                            <li style={{marginTop:"5px"}}>학습시간 : {flip_studied_study_hour}분</li>
                             <li style={{marginTop:"5px"}}>난이도1선택 : {flip_studied_diffi1}회</li>
                             <li style={{marginTop:"5px"}}>난이도2선택 : {flip_studied_diffi2}회</li>
                             <li style={{marginTop:"5px"}}>난이도3선택 : {flip_studied_diffi3}회</li>
